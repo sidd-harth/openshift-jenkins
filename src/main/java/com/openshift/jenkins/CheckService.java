2234ea16ad31731aaa226220a4b88a25165ea9b3
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-02-23 12:31:42 +0530

    uncomment for zero code smells

--- src/main/java/com/openshift/jenkins/CheckService.java
+++ src/main/java/com/openshift/jenkins/CheckService.java
@@ -5,8 +5,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class CheckService {
 
-String response = "Yeah, This service is deployed & it is running!!!!!!!!!!!!!!";
+
+    public String check() {
+        return "Yeah, This service is deployed & it is running!!!!!!!!!!!!!!";
+    }
+    
+    /* uncomment this to get zero code smells in Sonar Code Analysis 
+     
+    String response = "Yeah, This service is deployed & it is running!!!!!!!!!!!!!!";
     public String check() {
         return response;
     }
+     
+    */
+    
+    
 }
